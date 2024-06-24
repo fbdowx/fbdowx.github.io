9319de89904eac23ce9a921a1cae6f0dedc80b2f
fbdowx <151549159+fbdowx@users.noreply.github.com>
2024-06-24 14:06:16 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Register Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./pwa-sw.js')
+        navigator.serviceWorker.register('https://fbdowx.github.io/pwa-sw.js')
             .then(function (register) {
                 console.log('PWA service worker ready');
                 register.update();
